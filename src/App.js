637a31e557432656596dd994aa609ddc73dd00fb
Sofia Soerensen <sofia191004@gmail.com>
2025-04-19 16:39:17 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Sofia Soerensen</footer>
+      </div>
     </div>
   );
 }
-
-export default App;
